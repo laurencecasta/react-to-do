f63f4e3c3e73e2d2316ec1d78c282f0a5396eb65
Laurence Castaneda <laurence.d.castaneda@gmail.com>
2020-12-10 11:59:19 -0600

    Updates

---
+++ precache-manifest.24d721c3b085bfcb032d5d5438d7b963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684ef996a1408e9b136fc2c94bdeec54",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "703b4ce925b14321f011",
+    "url": "/react-to-do/static/css/main.2baa839f.chunk.css"
+  },
+  {
+    "revision": "3a86461b5c459e03835b",
+    "url": "/react-to-do/static/js/2.949d4bae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do/static/js/2.949d4bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "703b4ce925b14321f011",
+    "url": "/react-to-do/static/js/main.15cbac5d.chunk.js"
+  },
+  {
+    "revision": "49c2fb49ef8bffa32446",
+    "url": "/react-to-do/static/js/runtime-main.a602746f.js"
+  }
+]);
